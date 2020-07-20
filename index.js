1cf5e933f0a6bdf94c02793259304cbdb376073a
Tyler Hostler-Mathis <Hostler.Mathis.Tyler@gmail.com>
2020-07-19 20:59:17 -0400

    Working verion, first implementation

--- index.js
+++ index.js
@@ -3,46 +3,49 @@
 const fs = require('fs');
 let userData = require('./user_data.json');
 
+const userTemplate = require('./user_template.json');
+const catTemplate = require('./category_template.json');
 
 exports.addCategory = function(cat) {
 	if (exports.categoryExists(cat)) {
-		exports.logError('Category ' + cat + ' already exists.');
+		exports.logError('Category ' + cat + ' already exists');
 		return;
 	}
 
-	userData.categories.add(cat);
+	userData.categories.push(cat);
+	exports.log('Category ' + cat + ' has been created');
+	exports.saveUserData();
 }
 
 exports.incrementData = function(user, cat) {
 	if (!exports.categoryExists(cat)) {
-		console.logError('Category ' + cat + ' does not exist.');
-		return;
+		exports.logError('Category ' + cat + ' does not exist');
+		return false;
 	}
 
 	const validUser = exports.userExists(user);
 
 	if (validUser >= 0) {
-		const trackingCat = exports.userTrackingCat(validUser, cat);
-		if (trackingCat >= 0) {
-			userData.entries[validUser].data[trackingCat].count++;
+		const validCat = exports.userCatExists(validUser, cat);
+		if (validCat >= 0) {
+			userData.entries[validUser].data[validCat].count++;
 			exports.log(cat + ' was updated on user ' + user);
 		}
-		else {
-			exports.addUserCat(validUser, cat)
-			exports.log(cat + 'was added on user ' + user);
-		}
+		else
+			exports.addUserCat(validUser, user, cat)
 	}
 	else {
 		exports.addUser(user);
-		exports.log('New user ' + user + ' was created');
-		exports.addUserCat(userData.entries.length - 1, cat);
+		exports.addUserCat(userData.entries.length - 1, user, cat);
 	}
 
 	exports.saveUserData();
+	return true;
 }
 
 exports.categoryExists = function(cat) {
-	if (userData.categories.includes(cat)) return true;
+	if (userData.categories.includes(cat)) 
+		return true;
 	return false;
 }
 
@@ -53,7 +56,7 @@ exports.userExists = function(user) {
 	return -1;
 }
 
-exports.userTrackingCat = function(userIndex, cat) {
+exports.userCatExists = function(userIndex, cat) {
 	for (let category = 0; category < userData.entries[userIndex].data.length; category++)
 		if (userData.entries[userIndex].data[category].category === cat)
 			return category;
@@ -61,16 +64,60 @@ exports.userTrackingCat = function(userIndex, cat) {
 }
 
 exports.addUser = function(user) {
-	let newUser = require('./user_template.json');
-	newUser.name = user;
-	userData.entries.push(newUser);
+	userTemplate.name = user;
+	// string and parse for immutability
+	userData.entries.push(JSON.parse(JSON.stringify(userTemplate)));
+	exports.log('New user ' + user + ' was created');
 }
 
-exports.addUserCat = function(userIndex, cat) {
-	let newUserCat = require('./category_template.json');
-	newUserCat.category = cat;
-	newUserCat.count = 1;
-	userData.entries[userIndex].data.push(newUserCat);
+exports.addUserCat = function(userIndex, user, cat) {
+	catTemplate.category = cat;
+	catTemplate.count = 1;
+	// string and parse for immutability
+	userData.entries[userIndex].data.push(JSON.parse(JSON.stringify(catTemplate)));
+	exports.log('Category ' + cat + ' added to user ' + user);
+}
+
+exports.getUserData = function(client, target, user, cat) {
+	const validUser = exports.userExists(user);
+
+	if (validUser >= 0) {
+		const validCat = exports.userCatExists(validUser, cat);
+
+		if (validCat >= 0)
+			client.say(target, user + ' has been ' + cat + 'ed ' + userData.entries[validUser].data[validCat].count + ' times mrmedi6Smirk');
+		else {
+			exports.logError('Category ' + cat + ' is not registered on user ' + user);
+			return false;
+		}
+	}
+	else {
+		exports.logError('User ' + user + ' does not exist');
+		return false;
+	}
+	return true;
+}
+
+exports.removeUser = function(user) {
+	const validUser = exports.userExists(user);
+
+	exports.log(validUser);
+	if (validUser >= 0) {
+		userData.entries.splice(validUser, 1);
+		exports.log(user + ' has been removed');
+	}
+	else 
+		exports.logError(user + ' does not exist');
+}
+
+exports.resetUserData = function () {
+	const length = userData.entries.length;
+	for (let i = 0; i < length; i++) {
+		exports.log(userData.entries[0].name + ' has been removed');
+		userData.entries.splice(0, 1);
+	}
+	exports.saveUserData();
+	exports.log('All users have been removed');
 }
 
 exports.saveUserData = function(bak) {
@@ -85,7 +132,7 @@ exports.saveUserData = function(bak) {
 	      throw err;
 	    }
 	  });
-	exports.log('user_data.json has been udpated.')
+	exports.log('user_data.json has been udpated')
 }
 
 exports.log = function(msg) {
